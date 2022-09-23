dba959e7d0036f9cc2245fb07aa3faa122418c48
cf384 <cf384@M3>
2022-09-23 16:34:00 -0600

    Agregar archivos de proyecto.

---
+++ clDivision/clases/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace division
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cldivision odiv = new cldivision(double.Parse(textBox1.Text), double.Parse(textBox2.Text));
+            textBox3.Text = odiv.division().ToString();
+        }
+    }
+}
